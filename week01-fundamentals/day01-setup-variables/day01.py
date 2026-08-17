ca82c794440effa46a89e46762c72a44120bc6c1
Pinank Shah <shahpinank1411@gmail.com>
2026-08-17 08:53:58 +0530

    Week01 Day01: setup, variables, f-strings, about me

---
+++ week01-fundamentals/day01-setup-variables/day01.py
@@ -0,0 +1,13 @@
+print("Hellow World")
+print("My name is [Pinank Shah]")
+print("Today I am starting my python journey")
+
+# Variables
+age = 25
+name = "Pinank"
+print("I am", age,"years old")
+print(f"My name is {name} and I am {age} years old")
+
+city = "Mumbai"
+goal = "To Master Python"
+print(f"I live in {city} and my goal is {goal}")
